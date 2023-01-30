842d9f4771519766af3dbc4952118041efe7a290
fluxfw <fluxfw@fluxlabs.ch>
2023-01-30 13:09:04 +0100

    Remove build archive

--- src/autoload.php
+++ src/autoload.php
@@ -2,30 +2,8 @@
 
 namespace FluxFileStorageApi;
 
-require_once __DIR__ . "/../libs/flux-autoload-api/autoload.php";
-
-use FluxFileStorageApi\Libs\FluxAutoloadApi\Adapter\Autoload\Psr4Autoload;
-use FluxFileStorageApi\Libs\FluxAutoloadApi\Adapter\Checker\PhpExtChecker;
-use FluxFileStorageApi\Libs\FluxAutoloadApi\Adapter\Checker\PhpVersionChecker;
-
-PhpVersionChecker::new(
-    ">=8.2"
-)
-    ->checkAndDie(
-        __NAMESPACE__
-    );
-PhpExtChecker::new(
-    [
-        "zip"
-    ]
-)
-    ->checkAndDie(
-        __NAMESPACE__
-    );
-
-Psr4Autoload::new(
-    [
-        __NAMESPACE__ => __DIR__
-    ]
-)
-    ->autoload();
+spl_autoload_register(function (string $class) : void {
+    if (str_starts_with($class, __NAMESPACE__ . "\\")) {
+        require_once __DIR__ . str_replace("\\", "/", substr($class, strlen(__NAMESPACE__))) . ".php";
+    }
+});
